9793bb2e5e435b3f9c557ed48792e4b34e4686d5
ahmadatwi <ahmadatwi23@gmail.com>
2019-08-23 12:00:55 +0300

    #1 Upload the initial files of the feedback-services project
    In order to Upload the initial feedback-services project:
    1. Initialize a Spring project
    2. For now, the files are the default created ones

---
+++ feedback-services/src/main/java/dev/aatwi/feedback/management/tool/services/FeedbackServicesApplication.java
@@ -0,0 +1,15 @@
+package dev.aatwi.feedback.management.tool.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackServicesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FeedbackServicesApplication.class, args);
+    }
+
+}
