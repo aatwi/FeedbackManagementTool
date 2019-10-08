f32450a79e7536bffc7ff1d438ffdc087ebe2379
ahmadatwi <ahmadatwi23@gmail.com>
2019-10-08 17:20:15 -0400

    #2 Remove the usage of Flywaydb
    In order to enable users to LogIn to the application

--- fmt-services/src/main/java/dev/aatwi/fmtservices/config/PersistenceConfiguration.java
+++ fmt-services/src/main/java/dev/aatwi/fmtservices/config/PersistenceConfiguration.java
@@ -1,20 +1,31 @@
 package dev.aatwi.fmtservices.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration
 {
+    @Autowired
+    private Environment env;
+
+
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @Primary
     public DataSource getDataSource()
     {
-        return DataSourceBuilder.create().build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
     }
 
 }
