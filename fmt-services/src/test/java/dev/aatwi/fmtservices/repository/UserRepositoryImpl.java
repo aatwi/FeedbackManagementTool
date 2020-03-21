44ccb221f67463b1309e34713955ecd9a09137c9
ahmadatwi <ahmadatwi23@gmail.com>
2020-03-21 18:38:47 +0200

    Doing a POC on testing repository without starting Spring server

---
+++ fmt-services/src/test/java/dev/aatwi/fmtservices/repository/UserRepositoryImpl.java
@@ -0,0 +1,158 @@
+package dev.aatwi.fmtservices.repository;
+
+import dev.aatwi.fmtservices.model.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Optional;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    private final EntityManager em;
+    private final EntityManagerFactory emf;
+
+
+    public UserRepositoryImpl() {
+        emf = Persistence.createEntityManagerFactory("UserTest");
+        em = emf.createEntityManager();
+    }
+
+    @Override
+    public User findUserByEmailAndPassword(String email, String password) {
+
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAllById(Iterable<String> iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends User> iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends User> S save(S s) {
+        em.getTransaction().begin();
+        em.persist(s);
+        em.getTransaction().commit();
+
+//        Transaction transaction = currentSession.beginTransaction();
+//        currentSession.save(s);
+//        transaction.commit();
+        return s;
+    }
+
+    @Override
+    public <S extends User> List<S> saveAll(Iterable<S> iterable) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(String s) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends User> S saveAndFlush(S s) {
+        return null;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<User> iterable) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public User getOne(String s) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends User> boolean exists(Example<S> example) {
+        return false;
+    }
+}
--- fmt-services/src/test/java/dev/aatwi/fmtservices/repository/UserRepositoryTest.java
+++ fmt-services/src/test/java/dev/aatwi/fmtservices/repository/UserRepositoryTest.java
@@ -1,77 +1,77 @@
 package dev.aatwi.fmtservices.repository;
 
 import com.google.common.collect.Lists;
-import dev.aatwi.fmtservices.FmtServicesApplication;
 import dev.aatwi.fmtservices.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLException;
 
 import static dev.aatwi.fmtservices.model.UserBuilder.newUserBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-@SpringBootTest(classes = FmtServicesApplication.class)
-public class UserRepositoryTest
-{
-    @Autowired
+//@SpringBootTest(classes = FmtServicesApplication.class)
+public class UserRepositoryTest {
+    //    @Autowired
     UserRepository userRepository;
 
     User USER_A = newUserBuilder()
-        .withEmail("UserA@email.com")
-        .withName("User A")
-        .withPassword("UserAPassword")
-        .build();
+            .withEmail("UserA@email.com")
+            .withName("User A")
+            .withPassword("UserAPassword")
+            .build();
 
     User USER_B = newUserBuilder()
-        .withEmail("UserB@email.com")
-        .withName("User B")
-        .withPassword("UserBPassword")
-        .build();
-
+            .withEmail("UserB@email.com")
+            .withName("User B")
+            .withPassword("UserBPassword")
+            .build();
 
     @Test
     public void
-    it_should_return_the_corresponding_user_when_searching_by_email_and_password()
-    {
+    it_should_save_a_user() {
+//        this.userRepository.save(USER_A);
+    }
+
+    @Test
+    public void
+    it_should_return_the_corresponding_user_when_searching_by_email_and_password() {
         assertEquals(
-            USER_A,
-            userRepository.findUserByEmailAndPassword("UserA@email.com", "UserAPassword"));
+                USER_A,
+                userRepository.findUserByEmailAndPassword("UserA@email.com", "UserAPassword"));
     }
 
 
     @Test
     public void
-    it_should_return_the_corresponding_user_when_searching_by_email_and_password_regardless_of_case()
-    {
+    it_should_return_the_corresponding_user_when_searching_by_email_and_password_regardless_of_case() {
         assertEquals(
-            USER_A,
-            userRepository.findUserByEmailAndPassword("USERA@email.com", "UserAPassword"));
+                USER_A,
+                userRepository.findUserByEmailAndPassword("USERA@email.com", "UserAPassword"));
     }
 
 
     @Test
     public void
-    it_should_return_null_if_user_is_not_in_the_repository()
-    {
+    it_should_return_null_if_user_is_not_in_the_repository() {
         assertNull(userRepository.findUserByEmailAndPassword("UserC@email.com", "UserCPassword"));
     }
 
 
     @Test
     public void
-    it_should_return_all_the_users_when_getAllUsers_is_called()
-    {
+    it_should_return_all_the_users_when_getAllUsers_is_called() {
         assertEquals(Lists.newArrayList(USER_A, USER_B), userRepository.findAll());
     }
 
 
     @BeforeEach
     public void
-    setup()
-    {
+    setup() throws SQLException {
+        this.userRepository = new UserRepositoryImpl();
+
         userRepository.save(USER_A);
         userRepository.save(USER_B);
     }
@@ -79,8 +79,7 @@ public class UserRepositoryTest
 
     @AfterEach
     public void
-    deleteRepository()
-    {
+    deleteRepository() {
         userRepository.deleteAll();
     }
 }
